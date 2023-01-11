f467acf6456913ad7bf21c9af14ab25720b04b69
abhi-0086 <babaabhishek86@gmail.com>
2023-01-11 22:28:34 +0530

    data fetched from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+//import axios
+import axios from 'axios';
+
+//import icons
+import {
+  IoMdSunny,
+  IoMdRainy, 
+  IoMdCloudy, 
+  IoMdSnow, 
+  IoMdThunderstorm, 
+  IoMdSearch,
+} from 'react-icons/io';
+
+import { BsCloudHaze2Fill, BsCloudDrizzleFill, BsEye, BsWater, BsThermometer, BsWind } from'react-icons/bs';
+import { TbTemperatureCelsius } from 'react-icons/tb';
+import { ImSpinner8 } from 'react-icons/im';
+
+//api key
+const APIkey = '58ec3d77e151205f78f27b8d9816a99e';
+
+
+const App = () => {
+
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState('Udaipur');
+
+  //fetch data usng useEffect hook
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
+
+  //console log the data
+  // console.log(data);
+
+  //if the data is false aor there is any error then we have to show the loader
+  if(!data){
+    return(
+      <div>
+        <div>
+          <ImSpinner8 className='text-5xl animate-spin' />
+        </div>
+      </div>
+    )
+  }
+
+  //set the icon according toh the weather condition
+  let icon;
+  console.log(data.weather[0].main);//seeing data we get the weather condition data.weather[0].main as you see in console of data.weather
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>React App</div>
+  )
 }
 
 export default App;
